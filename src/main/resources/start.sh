c2c6d7f7efddc9ed882ded5fa33978ce1a991b24
TianHaoLiu <dlthliu@cn.ibm.com>
2018-06-26 14:41:34 +0800

    666

--- src/main/resources/start.sh
+++
@@ -1,3 +0,0 @@
-kill -9 `ps -ef | grep -o demo-0.0.1-SNAPSHOT.jar | cut -d ' ' -f 5`
-nohup java -Dfile.encoding=UTF-8 -jar ~/workspace/test/target/demo-0.0.1-SNAPSHOT.jar >~/workspace/log4.log &
-exit 0
