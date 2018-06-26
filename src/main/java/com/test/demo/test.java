82bcee5562a260c56543f044f475ed6c184c7e16
TianHaoLiu <dlthliu@cn.ibm.com>
2018-06-26 09:56:13 +0800

    first commit

---
+++ src/main/java/com/test/demo/test.java
@@ -0,0 +1,17 @@
+package com.test.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author THLiu
+ */
+@RestController
+public class test {
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public String test() {
+        return "Hello World";
+    }
+}
