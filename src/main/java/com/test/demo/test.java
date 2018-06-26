e21789370fb5afb0402fb79f347c9aeaf60a1189
TianHaoLiu <dlthliu@cn.ibm.com>
2018-06-26 16:12:49 +0800

    first commit

--- src/main/java/com/test/demo/test.java
+++ src/main/java/com/test/demo/test.java
@@ -12,6 +12,6 @@ public class test {
 
     @RequestMapping(value = "test", method = RequestMethod.GET)
     public String test() {
-        return "Hello World";
+        return "Hello World！！！！";
     }
 }
