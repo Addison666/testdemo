709fbc9db617c813692b786b170b39b857dafbb1
TianHaoLiu <dlthliu@cn.ibm.com>
2018-06-26 14:05:51 +0800

    666

--- src/main/resources/start.sh
+++ src/main/resources/start.sh
@@ -1,1 +1,1 @@
-nohup java -Dfile.encoding=UTF-8 -jar ~/workspace/test/target/demo-0.0.1-SNAPSHOT.jar &
+nohup java -Dfile.encoding=UTF-8 -jar ~/workspace/test/target/demo-0.0.1-SNAPSHOT.jar >log4.log &
